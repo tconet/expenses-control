22cf895e656b160ff9ec849d4229333614738e2e
tconet <tiagonet@gmail.com>
2022-07-04 14:03:28 -0300

    Versão Inicial, configurações de projeto, Eslint, Prettier, Typescript, etc.

---
+++ src/components/Soe.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+const Seo = () => {
+    return (
+        <Head>
+            <title>Expenses</title>
+            <meta name="Expenses Control" content="Expeneses Control" />
+        </Head>
+    )
+}
+export default Seo
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import Seo from 'src/components/Soe'
+
+const Home: NextPage = () => {
+  return <Seo></Seo>
+}
+export default Home
